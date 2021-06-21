d214785e4796e04ac7d465c3f472aaf50ac0b497
cemil <gokalpcakar@gmail.com>
2021-06-21 17:41:29 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace patika_try_catch_finally_pratigi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi: " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            // finally{
+            //     Console.WriteLine("İşlem Tamamlandı");
+            // }
+
+            try
+            {
+                // int a = int.Parse(null);
+                // int b = int.Parse("test");
+                int c = int.Parse("-20000000000000000000000000000000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş bir değer girdiniz.");
+                Console.WriteLine(ex);                
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);                
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz.");
+                Console.WriteLine(ex);                
+            }
+            finally{
+                Console.WriteLine("İşlem başarıyla tamamlandı");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Patika Odevi Icin Yaptigim Try-Catch-Finally Pratigi
+
+Patika.dev sitesi için yaptığım try-catch-finally pratiği içeren basit bir konsol uygulaması
